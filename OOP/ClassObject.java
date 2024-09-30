6bd8a0e2e24857683a064de7354120e7167f788c
Haakim19 <haakimajmal7@gmail.com>
2024-09-30 15:23:21 +0530

    OOP And Recursion

---
+++ OOP/ClassObject.java
@@ -0,0 +1,18 @@
+//main class
+public class ClassObject {
+    //Create a run method
+    public void run(String name) {
+        System.out.println(name + " can run faster");
+    }
+    //Create a eat method
+    public void eat(String name){
+        System.out.println(name + " eats a lot");
+    }
+
+    //inside the main, call the methods on the object
+    public static void main (String[] args){
+        ClassObject men = new ClassObject();
+        men.run("Zoro");
+        men.eat("Joyboy");
+    }
+}
