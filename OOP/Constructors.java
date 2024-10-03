37bfabc58c044e8c8b6836fa830b699a1a55ae02
Haakim19 <haakimajmal7@gmail.com>
2024-10-03 14:17:45 +0530

    COnstructors

--- OOP/Constructors.java
+++ OOP/Constructors.java
@@ -1,3 +1,14 @@
+class item{
+    public item(){ //? Default constructor
+        System.out.println("This is a constructor.");
+    }
+    public void display (){
+        System.out.println("This is a display method.");
+    }
+}
 public class Constructors {
-    
+    public static void main(String[] args) {
+        item item1 = new item();
+        item1.display();
+    }
 }
